4cdb0817d624590c1ed70ff4de6a2e58b093cbc2
kaseradeepak <deepak.kasera@scaler.com>
2026-07-04 15:48:45 +0530

    agents

--- stateful-agent.py
+++ stateful-agent.py
@@ -1,10 +1,12 @@
+# Build an agent to check the order status using the get_order_status tool. 
+# Use chat_history memory to help LLM retrieve the context for the previous conversation.
+
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.tools import tool
-from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
+from langchain_classic.agents import AgentExecutor, create_tool_calling_agent
 
-# Fake Orders DB
 ORDERS = {
     "ORD-101": {
         "status": "shipped",
@@ -53,18 +55,20 @@ prompt = ChatPromptTemplate.from_messages([
 You are a helpful customer support agent.
 
 Rules:
-- If the user gives an order id, keep a note of it.
-- If the user asks a query like "track the order" or "where is the order", use the order id from the input.
+- If the user gives an order id, remember it for this conversation. Give order information only if user is asking explicitly. 
+- If the user asks a follow-up like "track it" or "where is it", use the order id from chat history.
 - Use tools when order status is required.
 - If no order id is available, politely ask the user for the order id.
 """
     ),
 
-    # The previous conversation messages (HumanMessage + AIMessage) will be appened to the chat_history and will be used by the LLM to retrieve the context.
+    # previous message of the conversation will be inserted in the chat_history.
+    # optional=True makes the chat_history parameter as optional.
     MessagesPlaceholder(variable_name="chat_history", optional=True),
 
     ("human", "{input}"),
 
+    # Tool-Calling agent intermediate steps will be store here.
     MessagesPlaceholder(variable_name="agent_scratchpad")
 ])
 
@@ -78,16 +82,23 @@ agent = create_tool_calling_agent(
 
 agent_executor = AgentExecutor(
     agent=agent,
-    tools=tools
+    tools=tools,
+    verbose=True
 )
+
+chat_history = []
 
 def ask_agent(user_query: str):
     response = agent_executor.invoke(
         {
             "input" : user_query,
-            "chat_history" : []
+            "chat_history" : chat_history
         }
     )
+
+    # append the user_query and the AI response in the chat_history
+    chat_history.append(HumanMessage(content=user_query))
+    chat_history.append(AIMessage(content=response['output']))
 
     return response['output']
 
